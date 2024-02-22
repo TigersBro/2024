f08fe1f1abe0dcc91cad6fb85ccb9f44b13862be
TigersBro <gobles9638@outlook.com>
2024-02-22 16:54:41 -0500

    modified

--- 2024 Command Robot/src/main/java/frc/robot/Constants.java
+++ 2024 Command Robot/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,7 +14,34 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
-}
+    public static final class OIConstants {
+        public static final int kDriverController = 0;
+        public static final double kDriveDeadband = 0.05;
+        public static final double kArmManualDeadband = 0.05;
+        public static final double kArmManualScale = 0.5;
+    }
+    
+    public static final class Drivetrain {
+        public static final int kFrontLeftCanId = 1;
+        public static final int kFrontRightCanId = 2;
+        public static final int kRearLeftCanId = 3;
+        public static final int kRearRightCanId = 4;
+
+        public static final boolean kFrontLeftInverted = true;
+        public static final boolean kFrontRightInverted = false;
+        public static final boolean kRearLeftInverted = true;
+        public static final boolean kRearRightInverted = false;
+
+        public static final int kCurrentLimit = 55;
+
+        public static final double kTurningScale = 0.5;
+    }
+
+    
+
+        public static final double kSoftLimitReverse = 0.0;
+        public static final double kSoftLimitForward = 4.6;
+
+       
+
+       }
