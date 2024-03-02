432eac5fb778465f99145562bda0838200dd8ae8
OrangetheFruit <thegoldzombieplays@gmail.com>
2024-03-02 16:21:35 -0500

    put smartdashboard buttons into robot container

--- 2024 Command Robot/src/main/java/frc/robot/RobotContainer.java
+++ 2024 Command Robot/src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,12 @@ package frc.robot;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.commands.AutonomousCommand;
+import frc.robot.commands.IntakeCom;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -15,43 +20,65 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  //private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  //private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  public static RobotContainer m_robotContainer = new RobotContainer();
+
+  // The robot's subsystems and commands are defined here...
+  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  // private final ExampleCommand m_autoCommand = new
+  // ExampleCommand(m_exampleSubsystem);
 
   private final DrivetrainSubsystem m_drivetrain = new DrivetrainSubsystem();
-  private final Joystick m_driveController = new Joystick(Constants.OIConstants.kDriverController); 
+  private final Joystick m_driveController = new Joystick(Constants.OIConstants.kDriverController);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public final Arm m_arm = new Arm();
+  public final Intake m_intake = new Intake();
+  public final Shooter m_shooter = new Shooter();
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
+
+    // SmartDashboard Buttons
+    //SmartDashboard.putData("Autonomous Command", new AutonomousCommand());
+    //SmartDashboard.putData("Amp Pos", new AmpPos( m_arm ));
+    //SmartDashboard.putData("Speaker Pos", new SpeakerPos( m_arm ));
+    //SmartDashboard.putData("Shoot Amp", new ShootAmp( m_shooter ));
+    //SmartDashboard.putData("Shoot Speaker", new ShootSpeaker( m_shooter ));
+    SmartDashboard.putData("Intake Com", new IntakeCom( m_intake ));
+    //SmartDashboard.putData("Shoot", new Shoot( m_shooter ));
+    //SmartDashboard.putData("Prepare Intake", new PrepareIntake( m_arm ));
+
     // Configure the button bindings
-    configureButtonBindings(); 
+    configureButtonBindings();
+
+    
   }
 
-
   private void configureButtonBindings() {
-    //set up the drivetrain command that runs all the time
+    // set up the drivetrain command that runs all the time
     m_drivetrain.setDefaultCommand(new RunCommand(
-      () -> 
-        m_drivetrain.driveArcade(
-          MathUtil.applyDeadband(m_driveController.getY(), Constants.OIConstants.kDriveDeadband),
-          MathUtil.applyDeadband(-m_driveController.getZ()*Constants.Drivetrain.kTurningScale, Constants.OIConstants.kDriveDeadband))
-      , m_drivetrain)
-    );
+        () -> m_drivetrain.driveArcade(
+            MathUtil.applyDeadband(m_driveController.getY(), Constants.OIConstants.kDriveDeadband),
+            MathUtil.applyDeadband(-m_driveController.getZ() * Constants.Drivetrain.kTurningScale,
+                Constants.OIConstants.kDriveDeadband)),
+        m_drivetrain));
 
     SmartDashboard.putData(m_drivetrain);
 
-    
-    //set up arm manual and auto functions
+    // set up arm manual and auto functions
   }
 
   /**
--- 2024 Command Robot/src/main/java/frc/robot/commands/AutonomousCommand.java
+++ 2024 Command Robot/src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -7,13 +7,13 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
-public final class Autos {
+public final class AutonomousCommand {
   /** Example static factory for an autonomous command. */
   // public static Command exampleAuto(ExampleSubsystem subsystem) {
   //   //return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   // }
 
-  private Autos() {
+  private AutonomousCommand() {
     throw new UnsupportedOperationException("This is a utility class!");
   }
 }
